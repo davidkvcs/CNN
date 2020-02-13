de01af0a4c19d439213fa7a724df710b237bf2c3
David Kovacs <david.gergely.kovacs@regionh.dk>
2020-02-13 15:09:30 +0100

    comments on loss
    Loss functions that should be added

--- pythontoolkit/losses.py
+++ pythontoolkit/losses.py
@@ -2,3 +2,19 @@ from keras import backend as K
 
 def rmse(y_true, y_pred):
     return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
+
+
+#delineation loss
+# def delineation_loss_rmse(y_true, y_pred):
+# 	thres = 0.5
+# 	y_pred[y_pred[...,0] > thres] = 1
+# 	y_pred[y_pred[...,0] <= thres] = 0  
+#     return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
+
+#Dice loss
+
+#Haussdorff Loss
+
+#Absolute volume loss
+
+#Volumetric differences loss
